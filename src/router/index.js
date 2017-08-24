01659d735e0ab6879077b7766cf3a43094db2311
lv <771633433@qq.com>
2017-08-24 17:42:53 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
-Vue.use(Router)
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-export default new Router({
-  mode:'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+import Home from '../views/home.vue';
+import Lists from '../views/lists.vue';
+import Option from '../views/option.vue';
+import Detail from '../views/detail.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/lists', component: Lists },
+  { path: '/option', component: Option },
+  { path: '/detail', component: Detail },
+];
+
+const router = new VueRouter({routes});
+
+export default router;
